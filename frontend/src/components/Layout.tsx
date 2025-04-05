c5519e826a1dfc189382383c80be277211d92eef
Norvin Requeno <norvinrequeno@gmail.com>
2025-04-04 23:07:06 -0600

    Componente menu para aside

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,4 @@
-import React, { useState } from "react";
-import useAuth from "../auth/useAuth";
-import { Link } from "react-router-dom";
-import { ChevronLeft, LayoutDashboard, LogOut, Menu } from "lucide-react";
-import { LinkMenuType } from "../types";
+import Menu from "./Menu";
 
 export default function Layout({
   title = "POS",
@@ -11,73 +7,9 @@ export default function Layout({
   title?: string;
   children: React.ReactNode;
 }) {
-  const [open, setOpen] = useState(true);
-  const { name, logout } = useAuth();
-
-  const links: LinkMenuType[] = [
-    {
-      name: "Dashboard",
-      to: "/dashboard",
-      icon: <LayoutDashboard size={20} />,
-    },
-    {
-      name: "Cerrar sesión",
-      click: logout,
-      icon: <LogOut size={20} />,
-    },
-  ];
   return (
     <div className="flex h-screen bg-white">
-      <aside
-        className={`${
-          open ? "w-64" : "w-16"
-        } bg-white  transition-all duration-300 ease-in-out flex-shrink-0`}
-      >
-        <div className="h-full flex flex-col justify-between">
-          <div>
-            <div className="flex items-center justify-between p-4">
-              <span className="font-bold text-lg text-gray-700 uppercase">
-                {open && name}
-              </span>
-              <button onClick={() => setOpen(!open)}>
-                {open ? <ChevronLeft /> : <Menu />}
-              </button>
-            </div>
-
-            <nav className="space-y-1 mt-2">
-              {links.map((link: LinkMenuType, index) => {
-                if (link.to) {
-                  return (
-                    <Link
-                      key={index}
-                      to={link.to}
-                      className="flex items-center space-x-3 px-4 py-2 text-gray-700 hover:bg-amber-900 hover:text-white rounded"
-                    >
-                      <span>{link.icon}</span>
-                      {open && <span>{link.name}</span>}
-                    </Link>
-                  );
-                }
-
-                if (link.click) {
-                  return (
-                    <button
-                      key={index}
-                      onClick={link.click}
-                      className="flex items-center space-x-3 px-4 py-2 text-gray-700 hover:bg-amber-900 hover:text-white rounded w-full text-left"
-                    >
-                      <span>{link.icon}</span>
-                      {open && <span>{link.name}</span>}
-                    </button>
-                  );
-                }
-
-                return null;
-              })}
-            </nav>
-          </div>
-        </div>
-      </aside>
+      <Menu />
       <div className="flex-1 flex flex-col overflow-hidden">
         <header className="bg-white px-4 py-3 flex items-center justify-between">
           <div className="text-xl font-semibold text-gray-800">{title}</div>
---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import useAuth from "../auth/useAuth";
+import { LinkMenuType } from "../types";
+import { ChevronLeft, LayoutDashboard, LogOut, MenuIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+
+export default function Menu() {
+  const [open, setOpen] = useState(true);
+  const { name, logout } = useAuth();
+
+  const links: LinkMenuType[] = [
+    {
+      name: "Dashboard",
+      to: "/dashboard",
+      icon: <LayoutDashboard size={20} />,
+    },
+    {
+      name: "Cerrar sesión",
+      click: logout,
+      icon: <LogOut size={20} />,
+    },
+  ];
+  return (
+    <aside
+      className={`${
+        open ? "w-64" : "w-16"
+      } bg-white  transition-all duration-300 ease-in-out flex-shrink-0`}
+    >
+      <div className="h-full flex flex-col justify-between">
+        <div>
+          <div className="flex items-center justify-between p-4">
+            <span className="font-bold text-lg text-gray-700 uppercase">
+              {open && name}
+            </span>
+            <button onClick={() => setOpen(!open)}>
+              {open ? <ChevronLeft /> : <MenuIcon />}
+            </button>
+          </div>
+
+          <nav className="space-y-1 mt-2">
+            {links.map((link: LinkMenuType, index) => {
+              if (link.to) {
+                return (
+                  <Link
+                    key={index}
+                    to={link.to}
+                    className="flex items-center space-x-3 px-4 py-2 text-gray-700 hover:bg-amber-900 hover:text-white rounded"
+                  >
+                    <span>{link.icon}</span>
+                    {open && <span>{link.name}</span>}
+                  </Link>
+                );
+              }
+
+              if (link.click) {
+                return (
+                  <button
+                    key={index}
+                    onClick={link.click}
+                    className="flex items-center space-x-3 px-4 py-2 text-gray-700 hover:bg-amber-900 hover:text-white rounded w-full text-left"
+                  >
+                    <span>{link.icon}</span>
+                    {open && <span>{link.name}</span>}
+                  </button>
+                );
+              }
+
+              return null;
+            })}
+          </nav>
+        </div>
+      </div>
+    </aside>
+  );
+}
